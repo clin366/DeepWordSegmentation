e0d6784e1d13553be2018db0ae86e5536805b13e
yuchen <chenyu@firedata.cc>
2017-07-06 15:02:13 +0800

    Clean the text at the first

--- server/segmentation.py
+++ server/segmentation.py
@@ -11,9 +11,13 @@ from generate_file_segment import *
 from SegmentModel import *
 
 class segmentation:
-    def __init__(self, vec_path, parameters_path, crf_transition_matrix_path, model_path):
-        self.vec_dict = generate_vec_dict(vec_path)
-        self.parameter_dict = read_parameter(parameters_path)
+    def __init__(self):
+        self.vec_path = "./SegmentModel/vec.txt"
+        self.parameters_path = "./SegmentModel/parameters.txt"
+        self.crf_transition_matrix_path = "./SegmentModel/crf_transition_matrix.txt"
+
+        self.vec_dict = generate_vec_dict(self.vec_path)
+        self.parameter_dict = read_parameter(self.parameters_path)
 
         # Load the initial parameters
         self.max_sentence_len = self.parameter_dict["max_sentence_len"]
@@ -22,15 +26,15 @@ class segmentation:
         num_hidden = self.parameter_dict["num_hidden"]
 
         # Load the CRF transition matrix
-        self.crf_transition_matrix = np.loadtxt(crf_transition_matrix_path)
+        self.crf_transition_matrix = np.loadtxt(self.crf_transition_matrix_path)
 
         self.graph = tf.Graph()
         # Load the BI-LSTM + CRF model
         with self.graph.as_default():
-            self.model = SegmentModel(embedding_size, num_tags, vec_path, num_hidden, self.max_sentence_len)
+            self.model = SegmentModel(embedding_size, num_tags, self.vec_path, num_hidden, self.max_sentence_len)
             sv = tf.train.Saver()
             self.sess = tf.Session();
-            sv.restore(self.sess, model_path)
+            sv.restore(self.sess, './SegmentModel/SegmentModel')
             self.text_unary_score, self.text_sequence_length = self.model.test_unary_score()
 
     # Define the function to get the indxe of the character in the w2v
@@ -44,9 +48,7 @@ class segmentation:
         char_list = np.zeros([num_text, self.max_sentence_len], dtype = int)
 
         for i in range(len(text)):
-            temp_text = text[i].decode("utf-8").replace(" ","")
-            if len(temp_text) > 80:
-                temp_text = temp_text[:80]
+            temp_text = text[i].decode("utf-8")
             count = 0
             for char in temp_text:
                 if char.encode('utf-8') in self.vec_dict:
@@ -73,9 +75,6 @@ class segmentation:
         text = text.decode("utf-8")
         char_result = []
 
-        if len(text) > 80:
-            text = text[:80]
-            
         assert len(text) == len(result)
 
         for i in range(len(result)):
@@ -110,9 +109,13 @@ class segmentation:
         return final_result[0]
 
     def generate_final_result(self, text):
+        for i in range(len(text)):
+            if len(text[i]) > 80:
+                text[i] = text[i][:80]
+
         new_text = []
         for i in range(len(text)):
-            new_text.append(text[i].encode("utf-8"))
+            new_text.append(text[i].encode("utf-8").replace(" ",""))
 
         result = self.segment_text_without_filter(new_text)
         num_text = len(result)
