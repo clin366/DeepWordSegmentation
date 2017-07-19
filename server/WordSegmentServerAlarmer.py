e806c14b0ee2df9ccda2e7964153bec77b293434
曾祥能 <zengxiangneng@firedata.cc>
2017-07-19 16:44:22 +0800

    Update alarmer client

--- server/WordSegmentServerAlarmer.py
+++ server/WordSegmentServerAlarmer.py
@@ -9,50 +9,70 @@ from ai.botbrain.wordsegment import WordSegmentService
 from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
+from thrift.transport import TZlibTransport
+from thrift.protocol import TCompactProtocol
 
 import time
 import threading
+import traceback
 
 import loggingUtils
 import sendEmail
 
 logger = loggingUtils.my_logging.get_my_log("./logs/WordSegmentServerAlarmer.log", 'WordSegmentServerAlarmer')
 
-client = ""
+thriftClients = []
 t = ""
+
+class ThriftClient():
+    def __init__(self, ip, port):
+        logger.info('ip=' + str(ip) + ' port=' + str(port))
+        self.ip = ip
+        self.port = port
+        self.initConnect()
+
+    def initConnect(self):
+        socketTransport = TSocket.TSocket(self.ip, self.port)
+        self.transport = TZlibTransport.TZlibTransport(socketTransport)
+        self.transport.open()
+        protocol = TCompactProtocol.TCompactProtocol(self.transport)
+        self.client = WordSegmentService.Client(protocol)
+
+def closeClients():
+    global thriftClients
+    for thriftClient in thriftClients:
+        thriftClient.transport.close()
 
 def OnTimer():
     logger.info("OnTimer")
-    global client
-    try:
-        client.alive()
-        logger.info("心跳正常")
-    except Thrift.TException as tx:
-        logger.info('%s' % tx.message)
-        logger.info("分词服务掉线")
-        sendEmail.send_mail("分词服务掉线", "不好啦，出大事啦，分词服务掉线啦!")
-        raise tx
+    errorClients = []
+    global thriftClients
+    for thriftClient in thriftClients:
+        try:
+            thriftClient.client.alive()
+            logger.info("心跳正常")
+        except Thrift.TException as tx:
+            logger.error("分词服务掉线")
+            logger.error('%s' % tx.message)
+            logger.error(traceback.format_exc())
+            sendEmail.send_mail(str(thriftClient.ip) + " " + str(thriftClient.port) + "分词服务掉线", "不好啦，出大事啦，分词服务掉线啦!")
+            errorClients.append(thriftClient)
+
+    for errorClient in errorClients:
+        thriftClients.remove(errorClient)
+
     global t
     t = threading.Timer(30.0, OnTimer)
     t.start()
 
 def main():
-    # Make socket
-    transport = TSocket.TSocket(sys.argv[1], sys.argv[2])
-
-    # Buffering is critical. Raw sockets are very slow
-    transport = TTransport.TFramedTransport(transport)
-
-    # Wrap in a protocol
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-    # Create a client to use the protocol encoder
-    global client
-    client = WordSegmentService.Client(protocol)
-
-    # Connect!
-    transport.open()
+    i = 1
+    global thriftClients
+    logger.info("开始初始化client")
+    while i < len(sys.argv):
+        thriftClients.append(ThriftClient(sys.argv[i], sys.argv[i + 1]))
+        i += 2
+    logger.info("初始化所有client成功")
 
     global t
     t = OnTimer()
@@ -60,8 +80,7 @@ def main():
     while True:
         time.sleep(30)
 
-    # Close!
-    transport.close()
+    closeClients()
 
 if __name__ == '__main__':
     try:
